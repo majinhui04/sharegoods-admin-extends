6ca62b59c8709bb5cb10c16c5b2c5b043f52a743
guyu.j <jiangguyu@meeruu.com>
2019-10-14 10:15:26 +0800

    fix:下载请求超时时间可配置

--- src/utils/http/request.js
+++ src/utils/http/request.js
@@ -5,6 +5,7 @@ import configureTimeout from './interceptors/timeout';
 // import axiosRetry from 'axios-retry'
 
 class Request {
+    commonConfig = {}
     constructor(options) {
         const opt = {
             ...{
@@ -22,6 +23,7 @@ class Request {
             },
             ...options
         };
+        this.commonConfig = opt;
 
         const httpClient = axios.create({
             baseURL: opt.baseUrl,
@@ -122,8 +124,8 @@ class Request {
         const config = {
             url: url,
             method: method,
-            timeout: 20000,
-            responseType: 'arraybuffer' // blob
+            timeout: this.commonConfig.timeout || 20000,
+            responseType: "arraybuffer" // blob
         };
 
         if (params) {
